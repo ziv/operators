2cf1711f514a1c9c9a5b6d7808a2c4027736f427
Ziv <ziv.perry@zoominfo.com>
2021-12-19 12:09:18 +0200

    fix: test with async

--- src/filters/not-empty.spec.ts
+++ src/filters/not-empty.spec.ts
@@ -12,13 +12,16 @@ describe('not-empty', () => {
       });
   });
 
-  it('should not pass empty values', () => {
+  it('should not pass empty values', done => {
     const test: any[] = [];
     of([1, null, 2, undefined])
       .pipe(notEmpty$())
       .subscribe({
         next: value => test.push(value),
-        complete: () => expect(test.length).toEqual(2)
+        complete: () => {
+          expect(test.length).toEqual(2);
+          done();
+        }
       });
   });
 });
